d18c996b20ffe71cb9e50f3033301aad46f9e45c
BLACK <black>
2021-01-05 22:42:01 +0300

    сделал регистрацию

--- post_in/post_in/settings.py
+++ post_in/post_in/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'notes',
+
 ]
 
 MIDDLEWARE = [
@@ -68,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'post_in.wsgi.application'
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
