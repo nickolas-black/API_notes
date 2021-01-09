66531e891cd5ec4ad03e978d72c328d52bf406c7
BLACK <black>
2021-01-09 18:41:03 +0300

    вариант с классом

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from flask.views import MethodView
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["POST", "GET"])
 def index():
+    if request.method == "POST":
+        resp = request.get_json()
+        print(resp)
+        return '<h1> Hi BLACK!!! </h1>'
     return '<h1> Hi BOT!!! </h1>'
 
+class BotAPI(MethodView):
+
+    def get(self):
+        return '<h1> Hi BOT CLASS!!! </h1>'
+
+    def post(self):
+        resp = request.get_json()
+        print(resp)
+        return '<h1> Hi BLACK!!! </h1>'
+
+app.add_url_rule('/TOKEN/', view_func=BotAPI.as_view('bot'))
 
 if __name__ == '__main__':
     app.run()
